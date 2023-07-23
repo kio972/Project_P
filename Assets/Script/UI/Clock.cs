53567ed093722b78ef594f5b465996b0c6b2d8d0
KYJ <xpfjsdkdlel@naver.com>
2023-07-23 17:29:45 +0900

    Clock 수정
    Clock 일부 수정

--- Assets/Script/UI/Clock.cs
+++ Assets/Script/UI/Clock.cs
@@ -2,40 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Clock : MonoBehaviour
+namespace YeongJun
 {
-    [SerializeField]
-    private GameObject clock;
+    public class Clock : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject clock;
 
-    private Quaternion start;
-    private Quaternion end;
-    bool day = true;
+        private Quaternion start;
+        private Quaternion end;
+        bool day = true;
+        [SerializeField]
+        bool active = false;
 
-    public IEnumerator IChangeTime()
-    {// 시계가 회전하는 코루틴
-        float elapsedTime = 0;
-        float targetTime = 2.0f;
-        day = !day;
-        if (day)
-        {
-            start = Quaternion.Euler(0, 0, 180);
-            end = Quaternion.Euler(0, 0, 0);
-        }
-        else
-        {
-            start = Quaternion.Euler(0, 0, 360);
-            end = Quaternion.Euler(0, 0, 180);
-        }
-        while (elapsedTime < 2.0f)
-        {
-            elapsedTime += Time.deltaTime / targetTime;
-            clock.transform.rotation = Quaternion.Lerp(start, end, elapsedTime);
+        public IEnumerator IChangeTime()
+        {// 시계가 회전하는 코루틴
+            active = true;
+            float elapsedTime = 0;
+            float targetTime = 2.0f;
+            day = !day;
+            if (day)
+            {
+                start = Quaternion.Euler(0, 0, 180);
+                end = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                start = Quaternion.Euler(0, 0, 360);
+                end = Quaternion.Euler(0, 0, 180);
+            }
+            while (elapsedTime < targetTime)
+            {
+                elapsedTime += Time.deltaTime;
+                clock.transform.rotation = Quaternion.Lerp(start, end, elapsedTime / targetTime);
+                yield return null;
+            }
+            active = false;
             yield return null;
         }
-    }
 
-    public void ChangeTime()
-    {
-        StartCoroutine(IChangeTime());
+        public void ChangeTime()
+        {
+            if (!active)
+                StartCoroutine(IChangeTime());
+        }
     }
 }
