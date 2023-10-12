1efa2d4c9bbb7f3f38bc286fa28bca689fdafa4f
jun0433 <jubn0433@gmail.com>
2023-10-12 17:08:12 +0900

    오류 수정
    EventManager 추가
    exit script, btn 추가

--- Assets/Script/ClickAdvenScript.cs
+++ Assets/Script/ClickAdvenScript.cs
@@ -1,32 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickAdvenScript : MonoBehaviour
 {
-    public GameObject[] panels; // 패널들을 저장할 배열
-    private int currentPanelIndex = 0; // 현재 패널의 인덱스
+    public GameObject panels; // 패널들을 저장할 배열;
+    [SerializeField]
+    public Button AdvenBtn;
 
-    private void Start()
+
+
+    private void Update()
     {
-        ShowPanelAtIndex(currentPanelIndex);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HidePanel();
+        }
+        AdvenBtn.onClick.AddListener(ShowPanel);
     }
 
-    public void OnNextButtonClicked()
-    {
-        HidePanelAtIndex(currentPanelIndex); // 현재 패널 숨기기
 
-        currentPanelIndex = (currentPanelIndex + 1) % panels.Length; // 다음 패널 인덱스로 변경
-        ShowPanelAtIndex(currentPanelIndex); // 다음 패널 보이기
+    public void ShowPanel()
+    {
+        panels.SetActive(true);
     }
 
-    private void ShowPanelAtIndex(int index)
+    private void HidePanel()
     {
-        panels[index].SetActive(true);
-    }
-
-    private void HidePanelAtIndex(int index)
-    {
-        panels[index].SetActive(false);
+        panels.SetActive(false);
     }
 }
