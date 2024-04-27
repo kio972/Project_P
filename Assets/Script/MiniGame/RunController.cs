819550a5e20196171b39d1f2f0c504a65da885b8
jun0433 <jubn0433@gmail.com>
2024-04-27 22:13:44 +0900

    04.27 작업
    달리기 게임 UI 배치 완료
    달리기 게임 장애물 풀 작업 중

---
+++ Assets/Script/MiniGame/RunController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rig;
+    private bool isGround;
+
+    private bool game;
+
+    [SerializeField]
+    private float jumpPower = 3f;
+
+    private void Awake()
+    {
+        isGround = true;
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("isJump", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        game = RunManager.Inst.Start;
+        if(isGround)
+        {
+            anim.SetBool("isJump", false);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&isGround&&game)
+        {
+            isGround = false;
+            anim.SetBool("isJump", true);
+            rig.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+}
--- Assets/Script/MiniGame/RunManager.cs
+++ Assets/Script/MiniGame/RunManager.cs
@@ -1,12 +1,28 @@
+using JinWon;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
-public class RunManager : MonoBehaviour
+public class RunManager : Singleton<RunManager>
 {
     private GameObject back1;
     private GameObject back2;
+    private GameObject player;
+    private GameObject enemy;
+    private GameObject obj;
+
+    private int life;
+    private int score;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+
+    [SerializeField]
+    private GameObject gameFrame;
 
 
     [SerializeField]
@@ -14,10 +30,30 @@ public class RunManager : MonoBehaviour
     [SerializeField]
     private Vector3 startPos;
 
+    private bool start;
+    public bool Start
+    {
+        get => start;
+        set => start = value;
+    }
+
     private void Awake()
     {
+        start = false;
+        score = 0;
+        life = 3;
+
+        if (gameFrame == null)
+        {
+            Debug.Log("111");
+        }
+
+        //scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+
         back1 = GameObject.Find("Background1");
         back2 = GameObject.Find("Background2");
+        player = GameObject.Find("Player");
+        enemy = GameObject.Find("Enemy");
     }
 
     // Update is called once per frame
@@ -26,9 +62,36 @@ public class RunManager : MonoBehaviour
         back1.transform.position += scrollSpeed * Time.deltaTime * Vector3.left;
         back2.transform.position += scrollSpeed * Time.deltaTime * Vector3.left;
 
-        if(back1.transform.position.x <= -25.6)
+
+        if(enemy.transform.position.x < 8.8)
+        {
+            player.transform.position += 8f * Time.deltaTime * Vector3.right;
+            enemy.transform.position += 8f * Time.deltaTime * Vector3.right;
+        }
+        else if(enemy.transform.position.x >= 8.8 && life == 3)
+        {
+            start = true;
+        }
+
+        if(start)
+        {
+            gameFrame.SetActive(true);
+            score = score++;
+            scoreText.SetText("Score : " + score++ / 100);
+        }
+        else if(life <= 0 && !start)
+        {
+
+        }
+
+
+        if (back1.transform.position.x <= -25.6)
         {
             back1.transform.position = startPos;
+        }
+        if (back2.transform.position.x <= -25.6)
+        {
+            back2.transform.position = startPos;
         }
     }
 }
